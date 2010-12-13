47c290a9d7e80994a5f56ab8d5f45cdb75ddbeaf
J-N <J-N@John-Nicholas.Net>
2010-12-13 00:47:50 -0500

    tests
    Signed-off-by: J-N <J-N@John-Nicholas.Net>

---
+++ kernel/maxfiles.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+
+int rd_mkdir(char *pathname);
+int rd_creat(char *pathname);
+int rd_unlink(char *pathname);
+int rd_open(char * pathname);
+int rd_close(int fd, int pid);
+int rd_lseek(int fd, int offset);
+int rd_readdir(int fd, char *address);
+int rd_write(int fd, char *address, int num_bytes);
+int rd_read(int fd, char *address, int num_bytes);
+
+int main () 
+{
+	int i=0;
+	char* name= (char*)malloc(20);
+	for(i=0;i<1023;i++)
+	{
+		sprintf(name,"/file%d",i);
+		int result = rd_creat(name);
+		if(result != 0)
+		{
+			cout<<"Error creating file"<<i<<endl;
+		}
+	}
+	for(i=1022;i<=0;i--)
+	{
+		sprintf(name,"/file%d",i);
+		int result = rd_unlink(name);
+		if(result != 0)
+		{
+			cout<<"Error deleting file"<<i<<endl;
+		}
+	}
+	
+	return 0;
+}
---
+++ kernel/maxsize.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+
+int rd_mkdir(char *pathname);
+int rd_creat(char *pathname);
+int rd_unlink(char *pathname);
+int rd_open(char * pathname);
+int rd_close(int fd, int pid);
+int rd_lseek(int fd, int offset);
+int rd_readdir(int fd, char *address);
+int rd_write(int fd, char *address, int num_bytes);
+int rd_read(int fd, char *address, int num_bytes);
+
+int main () 
+{
+	int i=0;
+
+
+	int result = rd_creat("/bigfile");
+	if(result != 0)
+	{
+		cout<<"Error creating file"<<endl;
+	}
+	int fd = rd_open("/bigfile");
+	char* big = (char*)malloc(2013184);
+	while(strlen(big)<2013185)
+	{
+		sprintf(big,"%sJ",big);
+	}
+	int write = rd_write(fd,big,2013184);
+	cout<<"wrote "<<write<<" bytes"<<endl;
+	
+	char* bigr = (char*) malloc(2013184);
+	int read = rd_read(fd,bigr,2013184);
+	cout<<"read "<<read<<" bytes"<<endl;
+	int t;
+	cout<<"Enter 1 to see the results of read or 0 to skip"<<endl;
+	cin>>t;
+	if(t==1)
+	{
+		cout<<*bigr<<endl;
+	}
+	
+	
+	result = rd_unlink("/bigfile");
+	if(result != 0)
+	{
+		cout<<"Error deleting file"<<endl;
+	}
+	
+	return 0;
+}
